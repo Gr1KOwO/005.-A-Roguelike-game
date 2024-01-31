728561b8e610c8ed09fd1e10ea67a4e286bfe82b
Gr1KOwO <109365653+Gr1KOwO@users.noreply.github.com>
2024-01-31 10:42:48 +0300

    Update ChestScript.cs

--- TheBindingOfIsaac_Clone/Assets/Scripts/DungeonScripts/ChestScript.cs
+++ TheBindingOfIsaac_Clone/Assets/Scripts/DungeonScripts/ChestScript.cs
@@ -14,7 +14,7 @@ public class ChestScript : MonoBehaviour
     private class Item
     {
         public GameObject itemObject;
-        public float dropChance; // Шанс выпадения предмета (от 0 до 1)
+        public float dropChance; 
     }
 
     void Update()
@@ -34,29 +34,24 @@ public class ChestScript : MonoBehaviour
     private void OpenChest()
     {
         lid.SetActive(!isOpen);
-        // Суммируем все шансы
         float totalChance = 0f;
         foreach (Item item in items)
         {
             totalChance += item.dropChance;
         }
-        // Генерируем случайное число от 0 до суммарного шанса
+
         float randomChance = Random.Range(0f, totalChance);
         Debug.Log("Chance:"+ randomChance);
 
-        // Проходимся по всем предметам и выбираем предмет
         foreach (Item item in items)
         {
-            // Вычитаем шанс предмета из случайного числа
             randomChance -= item.dropChance;
 
-            // Если случайное число стало отрицательным, выбираем этот предмет
             if (randomChance <= 0f)
             {
-                // Создаем предмет и располагаем его в сундуке
                 GameObject itemSpawned = Instantiate(item.itemObject, spawnItem.position, Quaternion.identity);
                 itemSpawned.GetComponent<Rigidbody>().AddForce(-spawnItem.transform.forward* throwForce,ForceMode.VelocityChange);
-                break; // Прерываем цикл, так как предмет уже выбран
+                break;
             }
         }
     }
